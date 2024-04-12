85af587f0b8ca9672af0156076295c41779615e8
chermarey <slimix23@gmail.com>
2024-04-12 17:51:43 +0500

    add sharing

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ import '/backend/supabase/supabase.dart';
 import 'backend/firebase/firebase_config.dart';
 import 'flutter_flow/flutter_flow_util.dart';
 import 'flutter_flow/revenue_cat_util.dart' as revenue_cat;
+import 'package:flutter_sharing_intent/flutter_sharing_intent.dart';
+import 'package:flutter_sharing_intent/model/sharing_file.dart';
+import 'dart:async';
 
 import '/backend/firebase_dynamic_links/firebase_dynamic_links.dart';
 
@@ -56,6 +59,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   ThemeMode _themeMode = ThemeMode.system;
+  late StreamSubscription _intentDataStreamSubscription;
 
   late Stream<BaseAuthUser> userStream;
 
@@ -71,6 +75,33 @@ class _MyAppState extends State<MyApp> {
     userStream = flaresSupabaseUserStream()
       ..listen((user) => _appStateNotifier.update(user));
     jwtTokenStream.listen((_) {});
+
+
+    //Shared: getInitialMedia https://www.instagram.com/p/C5D8JKCNkOZ/?igsh=MTBoZ2M4N3RscmZwbw==
+    //should open Create_wish screen with url props
+    _intentDataStreamSubscription = FlutterSharingIntent.instance.getMediaStream()
+        .listen((List<SharedFile> value) {
+      final url = value.map((f) => f.value).join(",");
+
+      print("Shared: getMediaStream ${url}");
+
+      _router.go('/createWish?url=${Uri.encodeComponent(url)}');
+
+    }, onError: (err) {
+      print("getIntentDataStream error: $err");
+    });
+
+    // For sharing images coming from outside the app while the app is closed
+    FlutterSharingIntent.instance.getInitialSharing().then((List<SharedFile> value) {
+      print("Shared: getInitialMedia ${value.map((f) => f.value).join(",")}");
+
+      final url = value.map((f) => f.value).join(",");
+
+      if (url.isNotEmpty) {
+        _router.go('/createWish?url=${Uri.encodeComponent(url)}');
+      }
+    });
+
     Future.delayed(
       const Duration(milliseconds: 1000),
       () => _appStateNotifier.stopShowingSplashImage(),
@@ -80,6 +111,12 @@ class _MyAppState extends State<MyApp> {
   void setThemeMode(ThemeMode mode) => setState(() {
         _themeMode = mode;
       });
+
+  @override
+  void dispose() {
+    _intentDataStreamSubscription.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
