75baf81b6801d2ae84e501a5973525ee6fe845f4
Matvey Reshetnikov <slimix23@gmail.com>
2024-04-23 18:15:02 +0500

    fixes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -14,16 +14,16 @@ import flutter_sharing_intent
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
     
-    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
 
-        let sharingIntent = SwiftFlutterSharingIntentPlugin.instance
-        /// if the url is made from SwiftFlutterSharingIntentPlugin then handle it with plugin [SwiftFlutterSharingIntentPlugin]
-        if sharingIntent.hasSameSchemePrefix(url: url) {
-            print("lalallalalal", url);
-            return sharingIntent.application(app, open: url, options: options)
-        }
+    let sharingIntent = SwiftFlutterSharingIntentPlugin.instance
+    /// if the url is made from SwiftFlutterSharingIntentPlugin then handle it with plugin [SwiftFlutterSharingIntentPlugin]
+    if sharingIntent.hasSameSchemePrefix(url: url) {
+        print("lalallalalal", url);
+        return sharingIntent.application(app, open: url, options: options)
+    }
 
-        // Proceed url handling for other Flutter libraries like uni_links
-        return super.application(app, open: url, options:options)
-      }
+    // Proceed url handling for other Flutter libraries like uni_links
+    return super.application(app, open: url, options:options)
+  }
 }
